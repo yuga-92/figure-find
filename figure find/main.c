d3f36e35b7f651f1b72d6b3a8d4e049bfd29fa2a
Yuga Hal <YuGa@yugas-macbook-pro.local>
2015-11-16 18:42:26 +0200

    Initial Commit

---
+++ figure find/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  figure find
+//
+//  Created by Yuga Hal on 11/16/15.
+//  Copyright © 2015 Yuga Hal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
